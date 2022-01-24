4493c18e904ff8fab1a5113a9b0f9de6f6aadd69
Silvia Martinez <sil@silvia.com>
2022-01-24 19:37:28 +0100

    Added files js to all class of personajes

---
+++ js/Luchador.js
@@ -0,0 +1,33 @@
+import Personaje from "./Personaje";
+
+class Luchador extends Personaje {
+  arma;
+  destreza;
+
+  constructor(
+    nombreLuchador,
+    familiaLuchador,
+    edadLuchador,
+    armaLuchador,
+    valorDestreza
+  ) {
+    super(nombreLuchador, familiaLuchador, edadLuchador);
+    this.arma = armaLuchador;
+    this.destreza = this.filtraDestreza(valorDestreza);
+  }
+
+  filtraDestreza(valorDestreza) {
+    if (valorDestreza < 0) {
+      return 0;
+    }
+    if (valorDestreza > 10) {
+      return 10;
+    }
+    return this.valorDestreza;
+  }
+
+  comunicar() {
+    return `${super.comunicar()}Primero pego y luego pregunto`;
+  }
+}
+export default Luchador;
